27278eae0a88facbe554c144388ebee1cc3cfaae
ShevninEA <ev.shevnin@yandex.ru>
2022-12-17 17:57:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson_2__ASP_MVC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
